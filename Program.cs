5b20c00a2edd147dab6ab93cac9d8a7c89f25fde
Vladyslav Stovmanenko <flaviusglamfenix@gmail.com>
2018-08-25 14:19:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GMLParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
