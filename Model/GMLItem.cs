71d97a9f46afebe3dbdce4f8cc70211c4757c683
flaviusglamfenix@gmail.com <developer@localhost.localdomain>
2018-09-11 11:41:32 +0300

    Attached properties added.

--- Model/GMLItem.cs
+++ Model/GMLItem.cs
@@ -11,6 +11,9 @@ namespace GMLParser.Model
         public string NodeName { get; private set; }
         public Dictionary<string, string> ObjectProperties { get; } 
                                         = new Dictionary<string, string>();
+        
+        public Dictionary<string, string> AttachedProperties { get; }
+
         public List<Node> Children { get; } = new List<Node>();
         public Node(IEnumerable<string> tag)
         {
@@ -22,7 +25,7 @@ namespace GMLParser.Model
             var chars = tag.First().SkipWhile(c => c == ' ' || c == '<').TakeWhile(c => char.IsLetter(c));
             NodeName = new string(chars.ToArray());
 
-            Regex attributes = new Regex("[a-z,A-Z,0-9]{0,}[ ]{0,}[=]{1}[ ]{0,}[\"]{1}[a-z,A-Z,0-9]{0,}[\"]{1}");
+            Regex attributes = new Regex("[a-z,A-Z,0-9]{1,}[ ]{0,}[=]{1}[ ]{0,}[\"]{1}[a-z,A-Z,0-9]{0,}[\"]{1}");
 
             System.Text.StringBuilder tagStr = new System.Text.StringBuilder();
 
@@ -37,6 +40,16 @@ namespace GMLParser.Model
             {
                 string[] parts = attribute.Split('=');
                 ObjectProperties.Add(parts[0], parts[1].Trim('"'));
+            }
+
+            attributes = new Regex("[a-z,A-Z,0-9]{1,}[:]{1}[a-z,A-Z,0-9]{1,}[ ]{0,}[=]{1}[ ]{0,}[\"]{1}[a-z,A-Z,0-9]{0,}[\"]{1}");
+
+            attr = attributes.Matches(tagStr.ToString()).Select(m=>m.Value);
+
+            foreach(string attribute in attr)
+            {
+                string[] parts = attribute.Split('=');
+                AttachedProperties.Add(parts[0], parts[1].Trim('"'));
             }
         }
     }
